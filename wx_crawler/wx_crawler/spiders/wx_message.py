1f7b9bbb541bfcd658db03ece63601b0fb0760e3
zhjq <nkcs_zhjq@163.com>
2016-01-02 20:10:49 +0800

    1、修复了抓取文本不全的bug。之前只抓取了p标签下的文本，漏掉了section标签下的文本。
    但由于section标签下文本深度不同，通过xpath中的string()获取文本导致丢掉了文本之间是否在同一标签下的信息。
    
    2、现在抓取下来的文本和图片的文件命名采取日期_标题的格式。因为有的文章标题是相同的。

--- wx_crawler/wx_crawler/spiders/wx_message.py
+++ wx_crawler/wx_crawler/spiders/wx_message.py
@@ -5,7 +5,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 import codecs
-
+import re
 import scrapy
 
 
@@ -24,37 +24,35 @@ class WxMessageSpider(scrapy.Spider):
             self.parse_img(response)
 
     def parse_text(self, response):
-        head = response.xpath('//h2[@id="activity-name"]/text()').extract()[0].replace('\n','').replace('\r','').strip()
-        ps = response.xpath('//div[@id="img-content"]/div[2]/p')
-        content = '\r\n'.join([p.xpath('string(.)').extract()[0] for p in ps])
+        head = response.xpath('//h2[@id="activity-name"]/text()').extract()[0]
         time = response.xpath('//em[@id="post-date"]/text()').extract()[0]
-        auth = response.xpath('//a[@id="post-user"]/text()').extract()[0]
-        text = '\r\n'.join([head,time + ' ' + auth,content])
+        text = response.xpath('string(//div[@id="js_content"])').extract()[0]
+        text = re.sub(r'\r|\n|\s','',text)
         file_name = self.filter_file_name(head)
-        with codecs.open(file_name + '.txt','w',encoding='utf-8') as f:
+        with codecs.open(time + '_' + file_name + '.txt','w',encoding='utf-8') as f:
             f.write(text)
-        print file_name
+        # print file_name
 
     def parse_img(self, response):
-        head = response.xpath('//h2[@id="activity-name"]/text()').extract()[0].replace('\n','').replace('\r','').strip()
+        time = response.xpath('//em[@id="post-date"]/text()').extract()[0]
+        head = response.xpath('//h2[@id="activity-name"]/text()').extract()[0]
         head = self.filter_file_name(head)
-        div = response.xpath('//div[@id="img-content"]/div[2]')
+        div = response.xpath('//div[@id="js_content"]')
         imgs = div.xpath('.//img[@data-src]/@data-src').extract()
         for num,img in enumerate(imgs):
-            self.download_img(img, head+'-'+str(num)+'.jpeg')
+            self.download_img(img, time + '_' + head+'_'+str(num)+'.jpeg')
 
     def download_img(self, url, file):
         import urllib2
         req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat')
+        # req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat')
         resp = urllib2.urlopen(req)
         with open(file,'wb') as f:
             f.write(resp.read())
 
     def filter_file_name(self, file_name):
-        import re
         import os
         if os.name == 'nt':
-            return re.sub(r'\<|\>|\/|\\|\||\:|\"|\*|\?','',file_name)
+            return re.sub(r'\<|\>|\/|\\|\||\:|\"|\*|\?|\r|\n|\s','',file_name)
         else:
-            return re.sub(r'\/','',file_name)
+            return re.sub(r'\/|\r|\n|\s','',file_name)
--- wx_crawler/wx_crawler/spiders/wx_message_list.py
+++ wx_crawler/wx_crawler/spiders/wx_message_list.py
@@ -13,34 +13,35 @@ class WeixinSpider(scrapy.Spider):
     name = "weixin"
     allowed_domains = ["qq.com"]
     start_urls = (
-        'http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MzA4NDc2MTU2MQ==&uin=MTM4MjQxMDAyMw%3D%3D&key=41ecb04b05111003cb78720b68e2b4ff833b77e0c977681ceaf426c1f23fc304c83b9a7c4639e26c8c1833a6f6afa770&devicetype=Windows+7&version=61050021&lang=zh_CN&pass_ticket=daiVwVElSpDwTDJSMxOab7UIAeptN40pVSbMtGX%2BTrrvasGi7lpjzCnsBE9Uvuh3 HTTP/1.1',
+        'http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MjM5MjAxNDM4MA==&uin=MTM4MjQxMDAyMw%3D%3D&key=41ecb04b051110032c7441cbf9ca561cd13ecc01510163d62a95261e623d86b71a064452b41ab4e8b8195008c78ddd2d',
     )
 
     def __init__(self, img=False):
         self.message_crawler = WxMessageSpider(None, img)
 
     def parse(self, response):
-    	json_str = response.body.split("msgList = '")[1].split("'")[0].replace('&quot;','"')
-    	import json
-    	json_json = json.loads(json_str)
-    	lists = json_json['list']
-    	for item in lists:
-    		url = item['app_msg_ext_info']['content_url']
-    		yield scrapy.Request(url.replace('\\','').replace('amp;amp;','amp;'), callback=self.message_crawler.parse)
-    		subs = item['app_msg_ext_info']['multi_app_msg_item_list']
-    		for sub in subs:
-    			sub_url = sub['content_url']
-    			yield scrapy.Request(sub_url.replace('\\','').replace('amp;amp;','amp;'), callback=self.message_crawler.parse)
-
+        json_str = response.body.split("msgList = '")[1].split("'")[0].replace('&quot;','"')
+        import json
+        json_json = json.loads(json_str)
+        lists = json_json['list']
+        for item in lists:
+            message_id = item['comm_msg_info']['id']
+            url = item['app_msg_ext_info']['content_url']
+            yield scrapy.Request(url.replace('\\','').replace('amp;amp;','amp;'), callback=self.message_crawler.parse)
+            subs = item['app_msg_ext_info']['multi_app_msg_item_list']
+            for sub in subs:
+                sub_url = sub['content_url']
+                yield scrapy.Request(sub_url.replace('\\','').replace('amp;amp;','amp;'), callback=self.message_crawler.parse)
+        yield scrapy.Request(response.url.split('&f=text&frommsgid')[0]+"&f=text&frommsgid="+str(message_id)+"&count=10", callback=self.parse)
 
     # def parse_text(self, response):
-    # 	head = response.xpath('//h2[@id="activity-name"]/text()').extract()[0].replace('\n','').replace('\r','').strip()
+    #     head = response.xpath('//h2[@id="activity-name"]/text()').extract()[0].replace('\n','').replace('\r','').strip()
     #     ps = response.xpath('//div[@id="img-content"]/div[2]/p')
     #     content = '\r\n'.join([p.xpath('string(.)').extract()[0] for p in ps])
     #     time = response.xpath('//em[@id="post-date"]/text()').extract()[0]
     #     auth = response.xpath('//a[@id="post-user"]/text()').extract()[0]
     #     text = '\r\n'.join([head,time + ' ' + auth,content])
     #     with codecs.open(head+'.txt','w',encoding='utf-8') as f:
-    #     	f.write(text)
+    #         f.write(text)
     #     #下方print语句在linux下没问题，但是在windows下会有编码问题
     #     #print head
