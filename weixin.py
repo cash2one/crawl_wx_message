6641286ad748859d5f0a4d02ee1e052ae2f33447
zhjq <nkcs_zhjq@163.com>
2016-01-07 10:31:55 +0800

    SpecialUsers去重

--- weixin.py
+++ weixin.py
@@ -142,7 +142,7 @@ class WebWx():
         contacts = json.loads(resp.content)['MemberList']
         print '好友人数：%d'%(len(contacts))
 
-        SpecialUsers = ['newsapp', 'fmessage', 'filehelper', 'weibo', 'qqmail', 'fmessage', 'tmessage', 'qmessage', 'qqsync', 'floatbottle', 'lbsapp', 'shakeapp', 'medianote', 'qqfriend', 'readerapp', 'blogapp', 'facebookapp', 'masssendapp', 'meishiapp', 'feedsapp', 'voip', 'blogappweixin', 'weixin', 'brandsessionholder', 'weixinreminder', 'wxid_novlwrv3lqwv11', 'gh_22b87fa7cb3c', 'officialaccounts', 'notification_messages', 'wxid_novlwrv3lqwv11', 'gh_22b87fa7cb3c', 'wxitil', 'userexperience_alarm', 'notification_messages']
+        SpecialUsers = ["newsapp", "fmessage", "filehelper", "weibo", "qqmail", "tmessage","qmessage", "qqsync", "floatbottle", "lbsapp", "shakeapp","medianote", "qqfriend", "readerapp", "blogapp", "facebookapp","masssendapp", "meishiapp", "feedsapp", "voip", "blogappweixin","weixin", "brandsessionholder", "weixinreminder","wxid_novlwrv3lqwv11", "gh_22b87fa7cb3c", "officialaccounts","notification_messages", "wxitil", "userexperience_alarm"]
         for member in contacts:
             if member['VerifyFlag'] & 8 != 0: # 公众号/服务号
                 self.pub_accounts[member['UserName']] = member['NickName']
